1a33a2fcfaad448683a353aba79a609c12a4e09b
junwei <junwei.wang@boea-wisdom.com>
2023-11-24 11:40:36 +0800

    feat (junwei): code format - format code file mainwindow.h - format code file mainwindow.cpp

--- Src/Plots/mainwindow.h
+++ Src/Plots/mainwindow.h
@@ -47,54 +47,54 @@
 #include "QCustomPlot/QCustomPlot.h"
 
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 
 class MainWindow : public QMainWindow
 {
-  Q_OBJECT
-  
+    Q_OBJECT
+
 public:
-  explicit MainWindow(QWidget *parent = 0);
-  ~MainWindow();
-  
-  void setupDemo(int demoIndex);
-  void setupQuadraticDemo(QCustomPlot *customPlot);
-  void setupSimpleDemo(QCustomPlot *customPlot);
-  void setupSincScatterDemo(QCustomPlot *customPlot);
-  void setupScatterStyleDemo(QCustomPlot *customPlot);
-  void setupLineStyleDemo(QCustomPlot *customPlot);
-  void setupScatterPixmapDemo(QCustomPlot *customPlot);
-  void setupDateDemo(QCustomPlot *customPlot);
-  void setupTextureBrushDemo(QCustomPlot *customPlot);
-  void setupMultiAxisDemo(QCustomPlot *customPlot);
-  void setupLogarithmicDemo(QCustomPlot *customPlot);
-  void setupRealtimeDataDemo(QCustomPlot *customPlot);
-  void setupParametricCurveDemo(QCustomPlot *customPlot);
-  void setupBarChartDemo(QCustomPlot *customPlot);
-  void setupStatisticalDemo(QCustomPlot *customPlot);
-  void setupSimpleItemDemo(QCustomPlot *customPlot);
-  void setupItemDemo(QCustomPlot *customPlot);
-  void setupStyledDemo(QCustomPlot *customPlot);
-  void setupAdvancedAxesDemo(QCustomPlot *customPlot);
-  void setupColorMapDemo(QCustomPlot *customPlot);
-  void setupFinancialDemo(QCustomPlot *customPlot);
-  void setupPolarPlotDemo(QCustomPlot *customPlot);
-  
-  void setupPlayground(QCustomPlot *customPlot);
-  
+    explicit MainWindow(QWidget* parent = 0);
+    ~MainWindow();
+
+    void setupDemo(int demoIndex);
+    void setupQuadraticDemo(QCustomPlot* customPlot);
+    void setupSimpleDemo(QCustomPlot* customPlot);
+    void setupSincScatterDemo(QCustomPlot* customPlot);
+    void setupScatterStyleDemo(QCustomPlot* customPlot);
+    void setupLineStyleDemo(QCustomPlot* customPlot);
+    void setupScatterPixmapDemo(QCustomPlot* customPlot);
+    void setupDateDemo(QCustomPlot* customPlot);
+    void setupTextureBrushDemo(QCustomPlot* customPlot);
+    void setupMultiAxisDemo(QCustomPlot* customPlot);
+    void setupLogarithmicDemo(QCustomPlot* customPlot);
+    void setupRealtimeDataDemo(QCustomPlot* customPlot);
+    void setupParametricCurveDemo(QCustomPlot* customPlot);
+    void setupBarChartDemo(QCustomPlot* customPlot);
+    void setupStatisticalDemo(QCustomPlot* customPlot);
+    void setupSimpleItemDemo(QCustomPlot* customPlot);
+    void setupItemDemo(QCustomPlot* customPlot);
+    void setupStyledDemo(QCustomPlot* customPlot);
+    void setupAdvancedAxesDemo(QCustomPlot* customPlot);
+    void setupColorMapDemo(QCustomPlot* customPlot);
+    void setupFinancialDemo(QCustomPlot* customPlot);
+    void setupPolarPlotDemo(QCustomPlot* customPlot);
+
+    void setupPlayground(QCustomPlot* customPlot);
+
 private slots:
-  void realtimeDataSlot();
-  void bracketDataSlot();
-  void screenShot();
-  void allScreenShots();
-  
+    void realtimeDataSlot();
+    void bracketDataSlot();
+    void screenShot();
+    void allScreenShots();
+
 private:
-  Ui::MainWindow *ui;
-  QString demoName;
-  QTimer dataTimer;
-  QCPItemTracer *itemDemoPhaseTracer;
-  int currentDemoIndex;
+    Ui::MainWindow*             ui;
+    QString                     demoName;
+    QTimer                      dataTimer;
+    QCPItemTracer*              itemDemoPhaseTracer;
+    int                         currentDemoIndex;
 };
 
 #endif // MAINWINDOW_H
