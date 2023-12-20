bd713a976a2d756aef7ee693eac2435e7cab5dff
mc <wangjunwei1030@qq.com>
2023-12-20 23:43:21 +0800

    docs (mc): modify the comment in project API - API::initializePlot()

--- Src/API/API.cpp
+++ Src/API/API.cpp
@@ -36,15 +36,16 @@ void API::initializePlot()
 
     // 设置图形区域与图形内容区域的间距
     // 该属性可以不设置为 0
-    // 设置为 0 时会导致坐标轴最后一个 label 因为位置不够而不显示
-    //m_pPlot->axisRect()->setMargins(QMargins(0, 0, 0, 0));
-    //m_pPlot->axisRect()->setMinimumMargins(QMargins(0, 0, 0, 0));
+    // 上间距和由间距需要有足够的空间显示最上.最右的刻度 label
+    // 横坐标最大值比较大时。默认间距会导致最上、最右的刻度 label 无法显示
+    m_pPlot->axisRect()->setMargins(QMargins(0, 0, 0, 0));
+    m_pPlot->axisRect()->setMinimumMargins(QMargins(0, 0, 0, 0));
 
     // 设置坐标轴与图形内容区域的间距
     m_pPlot->xAxis->setPadding(0);
     m_pPlot->yAxis->setPadding(0);
 
-    m_pPlot->xAxis->setRange(0, 1.4);
+    m_pPlot->xAxis->setRange(0, 15000);
 
     // 刻度步长策略
     // tssReadability：可读性为主要的步长策略
